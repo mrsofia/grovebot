4ab43eda6de7431d23d5c2cbcb21dd1a8ad846b3
Justin Sofia <justin.sofia@bba12.mccombs.utexas.edu>
2016-05-24 21:38:15 -0500

    adds sqlite and peewee, and basic data persistence

--- Main.py
+++ Main.py
@@ -3,12 +3,32 @@ import telepot
 import time
 import soundcloud
 import spotipy
+import sqlite3
+from peewee import *
 
-TOKEN = open('TOKEN', 'r').read()
+# database setup
+db = SqliteDatabase('grovedb')
+
+TOKEN = 'your-token'
 # SCID = open('SOUNDCLOUDID', 'r').read()
 
 # scclient = soundcloud.Client(client_id=SCID)
 bot = telepot.Bot(TOKEN)
+
+
+class Link(Model):
+    user = CharField()
+    message = TextField()
+    # date = DateField()
+    time = TimeField()
+    link = CharField()
+
+    class Meta:
+        database = db
+
+db.connect()
+db.drop_tables([Link])
+db.create_tables([Link])
 
 
 def handle(msg):
@@ -28,7 +48,32 @@ def handle(msg):
             bot.sendMessage(chat_id, "haha this dumbass doesn't know how to send a spotify link I gotchu bro")
             bot.sendMessage(chat_id, url)
 
+            # capture spotify URLs in database
+            entry = Link.create(
+                user=msg.get('from').get('username'),
+                message=msg.get('text'),
+                time=msg.get('date'),
+                link=url
+            )
+            entry.save()
 
+        if "youtube.com/watch" in text or "soundcloud.com/" in text or "open.spotify.com" in text:
+            entry = Link.create(
+                user=msg.get('from').get('username'),
+                message=msg.get('text'),
+                time=msg.get('date')
+            )
+            entry.save()
+
+
+def save_link(msg):
+    pass
+
+def get_tracks_by_date(date):
+    pass
+
+def convert_spotify_uri(uri):
+    pass
 
 bot.message_loop(handle)
 
